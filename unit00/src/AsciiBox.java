e3c52cf3c6029d80d9f2bbec32be305ba053b2f4
avreylarson <avrey.larson@gmail.com>
2019-02-01 12:38:14 -0800

    AsciiArt

---
+++ unit00/src/AsciiBox.java
@@ -0,0 +1,33 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Avrey Larson
+//Date - 01/31/19
+//Class - APCSA p1
+//Lab  - 0a 1 
+
+public class AsciiBox
+{
+	public static void main(String[] args)
+	{
+		System.out.println("name Avrey Larson  date 01/31/10 \n\n" );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("AAAAAAAAAAAAAAAAAAAAAAAAA " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+		System.out.println("+++++++++++++++++++++++++ " );
+	}
+}
---
+++ unit00/src/Variables.java
@@ -0,0 +1,59 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name - Avrey Larson
+//Date - 01/31/19
+//Class - APCSA p1
+//Lab  - 0b 1 
+
+public class Variables
+{
+	public static void main ( String[] args )
+	{
+		//define 1 variable of each of the
+		//following data types
+		//byte		short		int 		long
+		//float		double
+		//char      boolean		String
+
+		//integer variables
+		byte byteOne = 127;
+		short shortOne = -5162001; 
+		int intOne = 1738; 
+		long longOne = 
+		float floatOne = 
+		double doubleOne = 
+		char charOne = 
+		boolean booleanOne = 
+		//decimal variables
+
+
+		//other integer types
+
+
+		//other types
+
+
+		//output your information here
+		System.out.println("/////////////////////////////////");
+		System.out.println("*                               *");
+		System.out.println("*        integer types          *");
+		System.out.println("*                               *");
+		System.out.println("*8 bit - byteOne = "+byteOne+"\t\t*");
+		System.out.println("*16 bit - shortOne = "+shortOne+"\t\t*");
+		System.out.println("*64 bit - intOne = "+intOne+"\t\t*");
+		System.out.println("*                               *");
+		System.out.println("*         real types           *");
+
+
+
+
+
+
+
+
+
+
+
+	}
+}
