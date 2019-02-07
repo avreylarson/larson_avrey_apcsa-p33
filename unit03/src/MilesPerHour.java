b1585811062f6468c338406d82b07a170c9f10fa
avreylarson <avrey.larson@gmail.com>
2019-02-07 13:06:41 -0800

    Update MilesPerHour

--- unit03/src/MilesPerHour.java
+++ unit03/src/MilesPerHour.java
@@ -23,18 +23,28 @@ public class MilesPerHour
 
 	public MilesPerHour(int dist, int hrs, int mins)
 	{
+		setNums(dist,hrs,mins);
+		mph = 0.0;
+		
 	}
 
 	public void setNums(int dist, int hrs, int mins)
 	{
+		distance = dist; 
+		hours = hrs; 
+		minutes = mins; 
 	}
 
 	public void calcMPH()
 	{
+		mph = (double)distance / (hours + (double)minutes/60);
 	}
 
 	public void print()
 	{
+		out.print("\n" + distance + " miles in " + hours + " hour and " + minutes + " minutes =\t");
+		out.print(Double.toString(mph));
+		out.print(" MPH");
 	}
 	
 	//create a print or toString or both
