a300b219b1022e8121d360141dca59cb366bb965
avreylarson <avrey.larson@gmail.com>
2019-02-07 12:44:14 -0800

    distance

--- unit03/src/DistanceRunner.java
+++ unit03/src/DistanceRunner.java
@@ -14,7 +14,28 @@ public class DistanceRunner
 {
 	public static void main( String[] args )
 	{
-		//add test cases	
+		Scanner keyboard = new Scanner (in); 
+		int x1, x2, y1, y2;
+		
+		for (int i=0; i<=3; i++) {
+			out.print("Please enter X1: ");
+			x1 = keyboard.nextInt(); 
+			
+			out.print("Please enter Y1: ");
+			y1 = keyboard.nextInt(); 
+			
+			out.print("Please enter X2: ");
+			x2 = keyboard.nextInt(); 
+			
+			out.print("Please enter Y2: ");
+			y2 = keyboard.nextInt(); 
+			
+			Distance trial = new Distance();
+			trial.calcDistance();
+			trial.print();
+			
+		}
+		
 			
 	}
 }
