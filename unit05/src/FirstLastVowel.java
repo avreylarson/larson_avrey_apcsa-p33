e0a9a82983f8e95fe03486cd85c8fa519f5acf79
avreylarson <avrey.larson@gmail.com>
2019-02-13 13:33:30 -0800

    vowels completion

--- unit05/src/FirstLastVowel.java
+++ unit05/src/FirstLastVowel.java
@@ -11,8 +11,11 @@ public class FirstLastVowel
 	   String beginning = a.substring(0, 1);
 	   String finish = a.substring(a.length()-1, a.length());
 	   
-	   if(finish.indexOf("aeiouAEIOU") == -1 || beginning.indexOf("aeiouAEIOU") == -1)
-		return "no";
-	}
-   		return "yes";
+	   String trial = "aeiouAEIOU";
+	   if(trial.indexOf(finish) >= 0 || trial.indexOf(beginning) >= 0) {
+		return "yes";
+	   }
+  
+   		return "no";
+} 
 }
