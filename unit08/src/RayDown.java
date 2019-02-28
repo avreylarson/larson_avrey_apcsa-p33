a3f84e4f29d70969d0b49f229323c2147359578d
avreylarson <avrey.larson@gmail.com>
2019-02-27 21:18:20 -0800

    8 changes

--- unit08/src/RayDown.java
+++ unit08/src/RayDown.java
@@ -1,14 +1,16 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-//Date -
-
 public class RayDown
 {
 	//go() will return true if all numbers in numArray
 	//are in decreasing order [31,12,6,2,1]
 	public static boolean go(int[] numArray)
 	{
-		return true;
+		boolean dec = true;
+		for(int i = 1; i < numArray.length - 1; i++) {
+			if(numArray[i] <= numArray[i+1]) {
+				dec = false;
+				break;
+			}
+		}
+		return dec;
 	}	
 }
