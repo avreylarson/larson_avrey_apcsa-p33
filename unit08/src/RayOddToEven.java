e5121fffc0f6549e3cf2b6173a532365f27c61f1
avreylarson <avrey.larson@gmail.com>
2019-02-27 20:41:43 -0800

    unit 8 completedddddd (fight on)

--- unit08/src/RayOddToEven.java
+++ unit08/src/RayOddToEven.java
@@ -1,12 +1,47 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-//Date -
-
 public class RayOddToEven
 {
-	public static boolean go(int[] ray)
+	public static int go(int[] ray)
 	{
-		return false;
+		int distance=0;
+		
+		for (int i=0;i<1;i++)
+		{
+			if ((ray[i]%2) != 0)
+			{ 
+				
+				for (int j=i;j<ray.length;j++)
+				{
+					if ((ray[j]%2)!=0)
+					{
+						distance++;
+					}
+					else
+						return distance;
+				}
+				
+			}
+			
+			
+			if ((ray[i]%2) == 0)
+			{ 
+				
+				for (int j=i;j<ray.length;j++)
+				{
+					if ((ray[j]%2)!=0)
+					{
+						distance++;
+					}
+					else
+						return distance;
+				}
+				
+			}
+			
+		
+		}
+			   
+		
+		return -1;
+		
 	}
 }
--- unit08/src/RaySumLast.java
+++ unit08/src/RaySumLast.java
@@ -1,12 +1,19 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-//Date -
-
 public class RaySumLast
 {
-	public static int go(int[] ray)
+	public int go(int[] ray)
 	{
-		return 0;
+		if (ray.length == 0)
+				return -1;
+		int val = ray[ray.length-1];
+		int sum = 0;
+		for(int i =0;i<ray.length-1;i++)
+		{
+			if(ray[i]>val)
+				sum = sum + ray[i];
+			
+		}
+		if (sum == 0)
+				return -1;
+		return sum;
 	}
 }
