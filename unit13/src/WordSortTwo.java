df7b08d4afaea55a595fcff972f6bd947d1d6b5e
avreylarson <avrey.larson@gmail.com>
2019-03-21 13:14:59 -0700

    13 done done

--- unit13/src/WordSortTwo.java
+++ unit13/src/WordSortTwo.java
@@ -1,7 +1,3 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import java.util.Arrays;
 import java.util.Scanner;
 import static java.lang.System.*;
@@ -12,15 +8,30 @@ public class WordSortTwo
 
 	public WordSortTwo(String sentence)
 	{
+		wordRay = sentence.split(" ");
 	}
 
 	public void sort()
 	{
+		int i = 1;
+		while (i < wordRay.length) {
+			int j = i; 
+			while (j > 0 && wordRay[j-1].compareTo(wordRay[j]) > 0) {
+				String temp = wordRay[j-1];
+				wordRay[j-1] = wordRay[j];
+				wordRay[j] = temp;
+				j--;
+			}
+			i++;
+		}
 	}
 
 	public String toString()
 	{
 		String output="";
+		for (String s: wordRay) {
+			output += s + "\n";
+		}
 		return output+"\n\n";
 	}
 }
--- unit13/src/WordSortTwoRunner.java
+++ unit13/src/WordSortTwoRunner.java
@@ -1,7 +1,3 @@
-//(c) A+ Computer Science
-//www.apluscompsci.com
-//Name -
-
 import java.util.Arrays;
 import java.util.Scanner;
 import java.io.File;
@@ -12,13 +8,15 @@ public class WordSortTwoRunner
 {
 	public static void main( String args[] ) throws IOException
 	{
-		Scanner file = new Scanner(new File("wordsorttwo.dat"));
+		Scanner file = new Scanner(new File("src//wordsorttwo.dat"));
 		int size = file.nextInt();
 		file.nextLine();
 		for(int i = 0; i<size; i++)
 		{
 			String sentence = file.nextLine();
-			//instantiate a new WordSort
+			WordSortTwo test = new WordSortTwo(sentence);
+			test.sort();
+			System.out.println(test);
 		}
 	}
 }
